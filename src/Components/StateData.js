8a03281a895a19d8d8ebf5bd992e9e919442c924
ravimishra99 <115026524+ravimishra99@users.noreply.github.com>
2024-08-14 15:20:36 +0530

    second commit

--- src/Components/StateData.js
+++ src/Components/StateData.js
@@ -3,65 +3,48 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const stateNameToCode = {
-   
-    'Andhra Pradesh': 'AP',
-    'Arunachal Pradesh': 'AR',
-    'Andaman and Nicobar Islands': 'AN',
-    'Assam': 'AS',
-    'Bihar': 'BR',
-    'Chandigarh': 'CH',
-    'Chhattisgarh': 'CT',
-    'Dadra and Nagar Haveli and Daman and Diu': 'DN',
-    'Delhi': 'DL',
-    'Goa': 'GA',
-    'Gujarat': 'GJ',
-    'Haryana': 'HR',
-    'Himachal Pradesh': 'HP',
-    'Jharkhand': 'JH',
-    'Jammu and Kashmir':'JK',
-    'Karnataka': 'KA',
-    'Kerala': 'KL',
-    'Ladakh': 'LA',
-    'Lakshadweep': 'LD',
-    'Madhya Pradesh': 'MP',
-    'Maharashtra': 'MH',
-    'Manipur': 'MN',
-    'Meghalaya': 'ML',
-    'Mizoram': 'MZ',
-    'Nagaland': 'NL',
-    'Odisha': 'OR',
-    'Puducherry': 'PY',
-    'Punjab': 'PB',
-    'Rajasthan': 'RJ',
-    'Sikkim': 'SK',
-    'Tamil Nadu': 'TN',
-    'Telangana': 'TG',
-    'Tripura': 'TR',
-    'Uttar Pradesh': 'UP',
-    'Uttarakhand': 'UT',
-    'West Bengal': 'WB'
+  'Andhra Pradesh': 'AP',
+  'Arunachal Pradesh': 'AR',
+  'Andaman and Nicobar Islands': 'AN',
+  'Assam': 'AS',
+  'Bihar': 'BR',
+  'Chandigarh': 'CH',
+  'Chhattisgarh': 'CT',
+  'Dadra and Nagar Haveli and Daman and Diu': 'DN',
+  'Delhi': 'DL',
+  'Goa': 'GA',
+  'Gujarat': 'GJ',
+  'Haryana': 'HR',
+  'Himachal Pradesh': 'HP',
+  'Jharkhand': 'JH',
+  'Jammu and Kashmir': 'JK',
+  'Karnataka': 'KA',
+  'Kerala': 'KL',
+  'Ladakh': 'LA',
+  'Lakshadweep': 'LD',
+  'Madhya Pradesh': 'MP',
+  'Maharashtra': 'MH',
+  'Manipur': 'MN',
+  'Meghalaya': 'ML',
+  'Mizoram': 'MZ',
+  'Nagaland': 'NL',
+  'Odisha': 'OR',
+  'Puducherry': 'PY',
+  'Punjab': 'PB',
+  'Rajasthan': 'RJ',
+  'Sikkim': 'SK',
+  'Tamil Nadu': 'TN',
+  'Telangana': 'TG',
+  'Tripura': 'TR',
+  'Uttar Pradesh': 'UP',
+  'Uttarakhand': 'UT',
+  'West Bengal': 'WB'
 };
 
-
 function StateData() {
-  const [data, setData] = useState(null);
   const [searchInput, setSearchInput] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://data.covid19india.org/v4/min/data.min.json');
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   useEffect(() => {
     if (searchInput) {
